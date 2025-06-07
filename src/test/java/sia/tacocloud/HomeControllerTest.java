be0802252a812aa1d7bdf769163570677b7f61b8
prparmar1 <161976176+PrpCena@users.noreply.github.com>
2025-06-07 14:08:24 -0400

    add HomeController, Home.html and HomeController.test

---
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package sia.tacocloud;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class) // web test for home controller
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; // injects MockMvc
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+}
